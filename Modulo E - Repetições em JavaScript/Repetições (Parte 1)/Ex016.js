9bcb2d548ecc4a339d3524edcdfbe73d9e97098e
Arthur Santos <art.bezerra2@gmail.com>
2025-09-21 19:04:16 -0300

    subindo arquivos aula 13

---
+++ Modulo E - Repetições em JavaScript/Repetições (Parte 1)/Ex016.js
@@ -0,0 +1,11 @@
+var contador = 0
+
+do{
+    console.log(`Do while: Contador = ${contador}`);
+    contador++;
+}while(contador < 3);
+
+while(contador <= 5){
+    console.log(`While simples: Contador = ${contador}`);
+    contador++;
+}
