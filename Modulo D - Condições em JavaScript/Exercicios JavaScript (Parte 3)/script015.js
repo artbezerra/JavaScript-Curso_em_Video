9c6e08ee3659f917d7bbe2c663fdf89baadf73f9
Arthur Santos <art.bezerra2@gmail.com>
2025-09-21 18:49:27 -0300

    arquivos aula 12 parte 3 completos

--- Modulo D - Condições em JavaScript/Exercicios JavaScript (Parte 3)/script015.js
+++ Modulo D - Condições em JavaScript/Exercicios JavaScript (Parte 3)/script015.js
@@ -15,12 +15,33 @@ function verificar(){
         
         if(user_sexo[0].checked){
             genero = 'homem'
-            resultado.innerHTML = `Você é um ${genero} de ${idade} anos.`
+
+            if(idade >= 0 && idade <= 13)
+                resultado.innerHTML = `Você é um menino com ${idade} anos.`
+        
+            else if(idade > 13 && idade <= 21)
+                resultado.innerHTML = `Você é um jovem com ${idade} anos.`
+                    
+            else if(idade > 21 && idade <= 50)
+                resultado.innerHTML = `Você é um adulto com ${idade} anos.`
+            
+            else if(idade > 50)
+                resultado.innerHTML = `Você é um idoso com ${idade} anos.`
         }
         else if (user_sexo[1].checked){
             genero = 'mulher'
-            resultado.innerHTML = `Você é uma ${genero} de ${idade} anos.`
+
+           if(idade >= 0 && idade <= 13)
+                resultado.innerHTML = `Você é uma menina com ${idade} anos.`
+        
+            else if(idade > 13 && idade <= 21)
+                resultado.innerHTML = `Você é uma jovem com ${idade} anos.`
+                    
+            else if(idade > 21 && idade <= 50)
+                resultado.innerHTML = `Você é uma adulta com ${idade} anos.`
+            
+            else if(idade > 50)
+                resultado.innerHTML = `Você é uma idosa com ${idade} anos.`
         }
     }
-
 }
