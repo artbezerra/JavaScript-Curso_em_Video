60e5dc876966a573369dbe84dd17de8ba08779b4
Arthur Santos <art.bezerra2@gmail.com>
2025-09-24 10:26:26 -0300

    adicioanndo arquivos exercicios parte 4 e 5

--- Modulo E - Repetições em JavaScript/Exercicios Javascript (Parte 4 e 5)/ex018.js
+++ Modulo E - Repetições em JavaScript/Exercicios Javascript (Parte 4 e 5)/ex018.js
@@ -1,10 +1,33 @@
-var hora = newDate().getHours()
-if (hora < 12) {
-    alert('Bom dia!')
-}
-else if (hora <= 18) {
-    alert('Boa tarde!')
-}
-else {
-    alert('Boa noite!')
+function contar() {
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var res = document.getElementById('res')
+    var i = Number(inicio.value)
+    var f = Number(fim.value)
+    var p = Number(passo.value)
+
+    if (inicio.value == 0 || fim.value == 0 || passo.value == 0) {
+        alert('[ERRO] Faltam dados!')
+    }
+    else {
+        res.innerHTML = 'Contando: <br>'
+
+        if (p > 0) {
+            for(var cont = i; cont <= f; cont += p) {
+                res.innerHTML += ` ${cont} \u{1F449}`
+                if (cont == f) {
+                    res.innerHTML += `\u{1F3C1}`
+                }
+            }
+        }
+        else if (p < 0) {
+            for(var cont = i; cont >= f; cont -= Math.abs(p)) {
+                res.innerHTML += ` ${cont} \u{1F449}`
+                if (cont == f) {
+                    res.innerHTML += `\u{1F3C1}`
+                }
+            }
+        }
+    }
 }
