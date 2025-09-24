7f374752a09d3760d28c6b1ff6d61bcd0091af42
Arthur Santos Bezerra <126301082+artbezerra@users.noreply.github.com>
2025-09-24 20:37:09 -0300

    Update ex023.js

--- Modulo F - Avaçando os Estudos em JavaScript/Exercicios Javascript (Parte 7 & 8)/ex023.js
+++ Modulo F - Avaçando os Estudos em JavaScript/Exercicios Javascript (Parte 7 & 8)/ex023.js
@@ -1,61 +1,58 @@
-let num = document.getElementById('num')
-let lista = document.getElementById('flista')
-let res = document.getElementById('res')
-let valores = []
+let num = document.getElementById('num');
+let lista = document.getElementById('flista');
+let res = document.getElementById('res');
+let valores = [];
 
 function ehNumero(n) {
-    if (Number(n) >= 1 && Number(n) <= 100) {
-        return true
-    } else {
-        return false
-    }
+  if (Number(n) >= 1 && Number(n) <= 100) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function estaNaLista(n, l) {
-    if (l.indexOf(Number(n)) != -1) {
-        return true
-    } else {
-        return false
-    }
+  if (l.indexOf(Number(n)) !== -1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function adicionar() {
-    if (ehNumero(num.value) && !estaNaLista(num.value, valores)) {
-        valores.push(Number(num.value))
-        let item = document.createElement('option')
-        item.text = `Valor ${num.value} adicionado.`
-        lista.appendChild(item)
-        res.innerHTML = ''
-    } else {
-        window.alert('Valor inválido ou já encontrado na lista.')
-    }
+  if (ehNumero(num.value) && !estaNaLista(num.value, valores)) {
+    valores.push(Number(num.value));
+    let item = document.createElement('option');
+    item.text = `Valor ${num.value} adicionado.`;
+    lista.appendChild(item);
+    res.innerHTML = '';
+  } else {
+    window.alert('Valor inválido ou já encontrado na lista.');
+  }
 }
 
 function finalizar() {
-    if (valores.length == 0) {
-        window.alert('Adicione valores antes de finalizar!')
-    } else {
-        let tot = valores.length
-        let maior = valores[0]
-        let menor = valores[0]
-        let soma = 0
-        let media = 0
-        for (let pos in valores) {
-            soma += valores[pos]
-            if (valores[pos] > maior)
-                maior = valores[pos]
-            if (valores[pos] < menor)
-                menor = valores[pos]
-        }
-        media = soma / tot
-        res.innerHTML = ''
-        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
-        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
-        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
-        res.innerHTML += `<p>A soma dos valores é ${soma}.</p>`
-        res.innerHTML += `<p>A média dos valores é ${media}.</p>`
+  if (valores.length === 0) {
+    window.alert('Adicione valores antes de finalizar!');
+  } else {
+    let tot = valores.length;
+    let maior = valores[0];
+    let menor = valores[0];
+    let soma = 0;
+    let media = 0;
+    for (let pos in valores) {
+      soma += valores[pos];
+      if (valores[pos] > maior) maior = valores[pos];
+      if (valores[pos] < menor) menor = valores[pos];
     }
-    num.value = ''
-    num.focus()
+    media = soma / tot;
+    res.innerHTML = '';
+    res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`;
+    res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`;
+    res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`;
+    res.innerHTML += `<p>A soma dos valores é ${soma}.</p>`;
+    res.innerHTML += `<p>A média dos valores é ${media}.</p>`;
+  }
+  num.value = '';
+  num.focus();
 }
-
